b980113fdb91fbde3a344afa1586f98f0b5be641
Rafael Silvério <rafael.silverio11@gmail.com>
2024-12-18 01:55:41 +0000

    Adding NOTICE file and respective licenses

---
+++ stundb/net/core/src/main/java/com/stundb/net/core/codecs/ObjectDecoder.java
@@ -0,0 +1,45 @@
+package com.stundb.net.core.codecs;
+
+import com.stundb.core.logging.Loggable;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ObjectDecoder extends ByteToMessageDecoder {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Codec codec;
+
+    public ObjectDecoder(Codec codec) {
+        setSingleDecode(false);
+        this.codec = codec;
+    }
+
+    @Loggable
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out)
+            throws IOException {
+        if (in == null || in.readableBytes() <= 0) {
+            return;
+        }
+
+        var bytes = new byte[in.readableBytes()];
+        in.readBytes(bytes);
+
+        try {
+            var obj = codec.decode(bytes);
+            if (obj != null) {
+                out.add(obj);
+            }
+        } catch (RuntimeException e) {
+            logger.error("Error decoding request", e);
+        }
+    }
+}
